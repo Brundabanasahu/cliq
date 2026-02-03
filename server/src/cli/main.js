02d97a0ee919a658340f41baf76a625fafd7388b
Brundabana Sahu <sbrundabana12@gmail.com>
2026-02-03 11:57:58 +0530

    Authentication are going ...

---
+++ server/src/cli/main.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import dotenv from "dotenv";
+import chalk from "chalk";
+import figlet from "figlet";
+
+import { Command } from "commander";
+
+dotenv.config();
+
+async function main(){
+    console.log(
+        chalk.cyan(
+            figlet.textSync("CLIQ",{
+                font:"Standard",
+                horizontalLayout:"default"
+            })
+        )
+    )
+    console.log(chalk.yellow("A CLI based AI tool\n"))
+
+    const program =new Command("orbital")
+
+    program.version("0.0.1")
+    .description("CLIQ - A CLI based AI Tool")
+
+    program.action(()=>{
+    program.help();
+    });
+
+    program.parse()
+}
+
+main().catch((err)=>{
+    console.log(chalk.red("Error in CLIQ: "),err)
+    process.exit(1)
+})
